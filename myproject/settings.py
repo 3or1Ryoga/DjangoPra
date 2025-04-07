e0c22bfbf25ec8a5ec1ed1294a4b3852c500d93b
Ryoga <sakanaliang@gmail.com>
2025-04-07 17:25:20 +0900

    templates/blogにテンプレートを追加しました

--- myproject/settings.py
+++ myproject/settings.py
@@ -122,4 +122,4 @@ STATIC_ROOT = BASE_DIR / 'static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['0.0.0.0','127.0.0.1', '.pythonanywhere.com']
